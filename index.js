05330fd784390c12d823edfd5fb737c18865e97d
zahaanshapoorjee <zahaan@shapoorjee.com>
2024-04-16 11:21:41 +0530

    ihml randomised annotations fetch

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001; 
-app.use(cors())
+app.use(cors({
+  origin: ['https://llmdatavalidator.netlify.app'],
+}));
 app.use(express.json()); 
 
 const db = mongoclient.db("Annotations");
